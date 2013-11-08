b507ff83af434ddda0c3d233a330fb7adeedfab4
Alex Groleau <alex@teampalmtree.com>
2013-11-08 17:52:26 -0500

    removed post/duration (handled in schedules UI)

--- classes/model/status.php
+++ classes/model/status.php
@@ -12,8 +12,6 @@ class Model_Status extends \Model
     // next file
     public $next_file_artist;
     public $next_file_title;
-    public $next_file_post;
-    public $next_file_duration;
     // current show
     public $current_show_title;
     public $current_show_duration;
